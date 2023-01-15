767d68912d8e7a9e7d18bdb26998ac3e01d52a72
lichangquan <lichangquan_w@inke.cn>
2023-01-15 13:27:54 +0800

    错误码处理完成

--- internal/middleware/logger.go
+++ internal/middleware/logger.go
@@ -2,80 +2,49 @@ package middleware
 
 import (
 	"fmt"
-	"os"
+	"math"
 	"time"
 
+	"github.com/CRORCR/call/internal/utils/log"
 	"github.com/gin-gonic/gin"
-	retalog "github.com/lestrrat-go/file-rotatelogs"
-	"github.com/rifflock/lfshook"
 	"github.com/sirupsen/logrus"
 )
 
-// 第一版本
 func Logger() gin.HandlerFunc {
-	filepath := "./logs/log.log"
-	src, err := os.OpenFile(filepath, os.O_RDONLY|os.O_CREATE|os.O_APPEND, 0755)
-
-	if err != nil {
-		fmt.Println("err:", err)
-	}
-
-	logger := logrus.New()
-	logger.Out = src
-	logger.SetLevel(logrus.DebugLevel)
-
-	logInfoWriter, _ := retalog.New(
-		"./logs/info-%Y%m%d%H.log",
-		retalog.WithMaxAge(7*24*time.Hour),
-		retalog.WithRotationTime(time.Hour),
-	)
-
-	writeMap := lfshook.WriterMap{
-		logrus.InfoLevel:  logInfoWriter,
-		logrus.DebugLevel: logInfoWriter,
-		logrus.WarnLevel:  logInfoWriter,
-		logrus.ErrorLevel: logInfoWriter,
-		logrus.PanicLevel: logInfoWriter,
-	}
-	Hook := lfshook.NewHook(writeMap, &logrus.TextFormatter{
-		TimestampFormat: "2006-01-02 15:04:05",
-	})
-	logger.AddHook(Hook)
-
-	return func(ctx *gin.Context) {
+	//entry := log.GetLogEntry(config.C.Logger.FilePath, config.C.Logger.FileName)
+	entry := log.GetLogEntry("logs", "latest")
+	return func(c *gin.Context) {
 		startTime := time.Now()
-		ctx.Next()
-		stopTime := time.Since(startTime).Microseconds()
-		spendTime := fmt.Sprintf("%d ms", stopTime)
-		clientIp := ctx.ClientIP()
-		//userAgent := ctx.Request.UserAgent()
-
-		method := ctx.Request.Method
-		path := ctx.Request.RequestURI
-		hostName, err := os.Hostname()
-		statusCode := ctx.Writer.Status()
-		if err != nil {
-			hostName = "unknown"
+		c.Next()
+		stopTime := time.Since(startTime)
+		spendTime := fmt.Sprintf("%d ms", int(math.Ceil(float64(stopTime.Nanoseconds()/1000000))))
+		statusCode := c.Writer.Status()
+		dataSize := c.Writer.Size()
+		if dataSize < 0 {
+			dataSize = 0
 		}
-		entry := logger.WithFields(logrus.Fields{
-			"host_name": hostName,
-			"status":    statusCode,
-			"cost":      spendTime,
-			"ip":        clientIp,
-			"method":    method,
-			"path":      path,
-			//"Agent":     userAgent,
+		clientIp := c.ClientIP()
+		userAgent := c.Request.UserAgent()
+		method := c.Request.Method
+		url := c.Request.RequestURI
+		l := entry.WithFields(logrus.Fields{
+			"SpendTime": spendTime,
+			"ClientIP":  clientIp,
+			"UserAgent": userAgent,
+			"Path":      url,
+			"Method":    method,
+			"Status":    statusCode,
+			"DataSize":  dataSize,
 		})
-
-		if len(ctx.Errors) > 0 {
-			entry.Error(ctx.Errors.ByType(gin.ErrorTypePrivate).String())
+		if len(c.Errors) > 0 {
+			l.Error(c.Errors.ByType(gin.ErrorTypePrivate))
 		}
 		if statusCode >= 500 {
-			entry.Error()
+			l.Error()
 		} else if statusCode >= 400 {
-			entry.Info()
+			l.Warn()
 		} else {
-			entry.Info()
+			l.Info()
 		}
 	}
 }
