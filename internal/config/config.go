1746a7019e8fd8d25c3d30d5a1167ef5bc9e2727
lichangquan <lichangquan_w@inke.cn>
2023-01-16 17:11:16 +0800

    配置文件拆分

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,137 +1,98 @@
 package config
 
-type Config struct {
-	App   ConfigApp   `yaml:"app"`
-	Redis ConfigRedis `yaml:"redis"`
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"strings"
+
+	"github.com/CRORCR/call/internal/model"
+	"github.com/jinzhu/configor"
+)
+
+type Configuration struct {
+	Conf *model.Config
 }
 
-type App struct {
-	Env  string `yaml:"env"`
-	Port uint64 `yaml:"port"`
+var (
+	GlobalConfig = Configuration{}
+)
+
+func InitConfig() {
+	config := flag.String("config", "", "Configuration file")
+	flag.Parse()
+	if config == nil {
+		panic("Please enter Configuration file")
+	}
+	switch *config {
+	case EnvProduction, EnvTesting, EnvDevelopment:
+	default:
+		panic("Please enter Configuration file")
+	}
+	fmt.Println("--------------")
+	fmt.Println("./config/" + *config)
+	fmt.Println("./config/")
+	//GlobalConfig.LoadConfig("./config/"+*config+"/", "")
+	//GlobalConfig.LoadConfig("./config/", "")
+	GlobalConfig.LoadConfig("./config/testing/", "")
+	v, _ := json.Marshal(GlobalConfig)
+	fmt.Println("读取---", string(v))
 }
 
-type ConfigDB struct {
-	User ConfigMysql `yaml:"user"`
+func (c *Configuration) LoadConfig(configFilePtr string, secretFilePtr string) {
+	configPath := ``
+	secretPath := ``
+	if len(configFilePtr) == 0 {
+		configPath = "./config/"
+	} else {
+		configPath = configFilePtr
+	}
+	if len(secretFilePtr) == 0 {
+		secretPath = "./secret/"
+	} else {
+		secretPath = secretFilePtr
+	}
+	configfiles := GetConfigFiles(configPath, secretPath)
+	c.Conf = new(model.Config)
+
+	// 从配置文件中加载
+	err := configor.Load(c.Conf, configfiles...)
+	if err != nil {
+		msg := "Failed to load config file !!! " + err.Error()
+		panic(msg)
+	}
 }
 
-type ConfigApp struct {
-	ENV  string `yaml:"env"`
-	Port string `yaml:"port"`
+func GetConfigFiles(dirs ...string) []string {
+	configfiles := make([]string, 10)
+	for i := 0; i < len(dirs); i++ {
+		dir := dirs[i]
+		configfiles = walkDir(configfiles, dir)
+	}
+
+	return deleteEmpty(configfiles)
 }
 
-type ConfigRedis struct {
-	Host string `yaml:"host"`
-	Port int    `yaml:"port"`
-	DB   int    `yaml:"db"`
+func walkDir(configfiles []string, dirname string) []string {
+	files, err := ioutil.ReadDir(dirname)
+	if err == nil {
+		for _, f := range files {
+			if strings.Contains(f.Name(), ".yaml") {
+				configfiles = append(configfiles, dirname+f.Name())
+				fmt.Println("加入的文件----", dirname+f.Name())
+			}
+		}
+	}
+	return configfiles
 }
 
-type ConfigMysql struct {
-	Host          string `yaml:"host"`
-	Username      string `yaml:"username"`
-	Password      string `yaml:"password"`
-	Database      string `yaml:"database"`
-	Port          uint32 `yaml:"port"`
-	IsAutoMigrate bool   `yaml:"is_auto_migrate"`
-	LogMode       bool   `yaml:"log_mode"`
-	MaxIdleConns  int    `yaml:"max_idle_conns"`
-	MaxOpenConns  int    `yaml:"max_open_conns"`
+func deleteEmpty(configfiles []string) []string {
+	var retConfigfiles []string
+	for _, configfile := range configfiles {
+		if configfile != "" {
+			retConfigfiles = append(retConfigfiles, configfile)
+		}
+	}
+	return retConfigfiles
 }
-
-//
-//
-
-//
-//type Mysql struct {
-//	Name string `yaml:"name"`
-//}
-//
-//func InitConfig() {
-//	config := flag.String("config", "", "Configuration file")
-//	flag.Parse()
-//	if config == nil {
-//		panic("Please enter Configuration file")
-//	}
-//	switch *config {
-//	case EnvProduction, EnvTesting, EnvDevelopment:
-//	default:
-//		panic("Please enter Configuration file")
-//	}
-//
-//	GlobalConfig.LoadConfig(nil, nil)
-//	v, _ := json.Marshal(GlobalConfig)
-//	fmt.Println("读取---", string(v))
-//
-//	paramsConfig(*config)
-//}
-//
-//func paramsConfig(configEnv string) {
-//	mysql := new(Mysql)
-//	app := new(App)
-//	var conf = &Config{}
-//	conf.App = app
-//	conf.Mysql = mysql
-//
-//	envFolder := "./config/" + configEnv
-//	// 找到文件夹下面所有文件
-//	// 每一个文件读取，并反序列化成对应结构体
-//	// 检查文件夹是否存在
-//	if _, err := os.Stat(envFolder); os.IsNotExist(err) {
-//		panic(fmt.Sprintf("folder %s not exist,err:%v", envFolder, err))
-//	}
-//
-//	// 读取每个文件
-//	files, err := ioutil.ReadDir(envFolder)
-//	if err != nil {
-//		panic("文件不存在")
-//	}
-//
-//	for _, file := range files {
-//		if !strings.Contains(file.Name(), "app") {
-//			continue
-//		}
-//		fmt.Println("找到app文件", file.Name())
-//
-//		loadVal, err := loadConfigFile(envFolder, file.Name())
-//		if err != nil {
-//			panic(err)
-//		}
-//
-//		fileName := strings.Split(file.Name(), ".")
-//		if len(fileName) == 0 {
-//			continue
-//		}
-//		fmt.Println("--jhhh--", string(loadVal))
-//		switch fileName[0] {
-//		case "app":
-//			if err = json.Unmarshal(loadVal, app); err != nil {
-//				panic(err)
-//			}
-//		}
-//		fmt.Println("-------", *app)
-//	}
-//}
-//
-//func loadConfigFile(folder string, file string) ([]byte, error) {
-//	//  判断文件是否以yaml或者yml作为后缀
-//	s := strings.Split(file, ".")
-//	if len(s) != 2 || s[1] != "yaml" {
-//		return nil, fmt.Errorf(" %s is illegal", file)
-//	}
-//
-//	// 读取文件内容
-//	bf, err := ioutil.ReadFile(filepath.Join(folder, file))
-//	if err != nil {
-//		return nil, fmt.Errorf("read %s failed,err:%v", file, err)
-//	}
-//	fmt.Println("读物文件", string(bf))
-//	// 直接针对文本做环境变量的替换
-//	//bf = replace(bf, conf.envMaps)
-//	// 解析对应的文件
-//	c := make(map[string]interface{}, 0)
-//	if err = yaml.Unmarshal(bf, &c); err != nil {
-//		return nil, fmt.Errorf("read %s failed,err:%v", file, err)
-//	}
-//
-//	conf, _ := json.Marshal(c)
-//	return conf, nil
-//}
